99c9a91e9427dfa7cd73b44f8b91f9a9bffb2f11
Sergei Korotaev <zergwsmt@gmail.com>
2025-05-31 15:50:48 +0300

    Task 2.1 - Login.

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -1,10 +1,17 @@
 package ru.otus.coroutineshomework.ui.login
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
+import ru.otus.coroutineshomework.ui.login.data.User
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(private val loginApi: LoginApi = LoginApi()) : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
@@ -15,13 +22,46 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingIn
+            Log.i("login state", _state.value.toString())
+            try {
+                if (name == "main") {
+                    loginApi.login(Credentials(name, password))
+                }
+                else {
+                    val user: User = withContext(Dispatchers.IO) {
+                        loginApi.login(Credentials(name, password))
+                    }
+                    _state.value = LoginViewState.Content(user)
+                    Log.i("login state", _state.value.toString())
+                }
+            }
+            catch (exception: Exception){
+                _state.value = LoginViewState.Login(error = exception)
+                Log.i("login state", _state.value.toString())
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingOut
+            Log.i("login state", _state.value.toString())
+            try {
+                withContext(Dispatchers.IO) {
+                    loginApi.logout()
+                }
+                _state.value =LoginViewState.Login()
+                Log.i("login state", _state.value.toString())
+            }
+            catch (exception: Exception){
+                _state.value = LoginViewState.Login(error = exception)
+                Log.i("login state", _state.value.toString())
+            }
+        }
     }
 }
